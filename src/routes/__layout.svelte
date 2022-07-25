63329220223db2157afc3865a2b554b26cc6fdbc
Sam Nystrom <samuel.l.nystrom@gmail.com>
2022-07-25 00:41:52 -0400

    setup

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
+  import IconButton from '@smui/icon-button';
+</script>
+
+<TopAppBar>
+  <Row>
+    <Section>
+      <IconButton class="material-icons">menu</IconButton>
+      <Title>Thing</Title>
+    </Section>
+  </Row>
+</TopAppBar>
+
+<slot />
