866a1a22327948df4148e54dd02ec6200162369c
Sam Nystrom <samuel.l.nystrom@gmail.com>
2022-07-28 14:03:17 -0400

    add SVG animations

---
+++ src/lib/HabitatLoss.svelte
@@ -0,0 +1,106 @@
+<svg width="158" height="116" viewBox="0 0 158 116" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect id="background" width="158" height="116" fill="#428A45" />
+  <path
+    id="road"
+    style="--order: 0"
+    d="M34.5 68C18.0462 74.1702 3.16667 95.1667 0 100V115.5C2.16667 108.833 21.5337 83.9443 43 75C61 67.5 84.9235 68.6386 107 60C130 51 144.167 17.8333 149.5 0.5H139C136.833 13.6667 125.8 41 101 51C70 63.5 58.5 59 34.5 68Z"
+    fill="#484848"
+    clip-path="url(#clip)"
+  />
+  <g id="houses" style="--order: 1" clip-path="url(#clip)">
+    <g>
+      <rect
+        x="49"
+        y="75.8755"
+        width="26"
+        height="18"
+        transform="rotate(-8.57227 49 75.8755)"
+        fill="#836655"
+      />
+      <rect
+        x="49"
+        y="75.8755"
+        width="26"
+        height="18"
+        transform="rotate(-8.57227 49 75.8755)"
+        fill="#836655"
+      />
+    </g>
+    <g>
+      <rect
+        x="110"
+        y="63.2121"
+        width="26"
+        height="18"
+        transform="rotate(-38.5753 110 63.2121)"
+        fill="#836655"
+      />
+      <rect
+        x="110"
+        y="63.2121"
+        width="26"
+        height="18"
+        transform="rotate(-38.5753 110 63.2121)"
+        fill="#836655"
+      />
+    </g>
+    <g>
+      <rect
+        x="9.68289"
+        y="56.7937"
+        width="26"
+        height="18"
+        transform="rotate(-29.4988 9.68289 56.7937)"
+        fill="#836655"
+      />
+      <rect
+        x="9.68289"
+        y="56.7937"
+        width="26"
+        height="18"
+        transform="rotate(-29.4988 9.68289 56.7937)"
+        fill="#836655"
+      />
+    </g>
+    <g>
+      <rect
+        x="70"
+        y="38.4644"
+        width="26"
+        height="18"
+        transform="rotate(-16.684 70 38.4644)"
+        fill="#836655"
+      />
+      <rect
+        x="70"
+        y="38.4644"
+        width="26"
+        height="18"
+        transform="rotate(-16.684 70 38.4644)"
+        fill="#836655"
+      />
+    </g>
+  </g>
+</svg>
+
+<style>
+  #road {
+    animation: fade 4s linear infinite;
+  }
+  #houses {
+    animation: fade 4s linear infinite;
+    animation-delay: 200ms;
+  }
+  @keyframes fade {
+    0% {
+      opacity: 0;
+    }
+    15%,
+    81% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+</style>
---
+++ src/lib/SmokeStack.svelte
@@ -0,0 +1,54 @@
+<svg width="57" height="179" viewBox="0 -20 57 159" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <g id="smoke">
+    <circle id="smoke3" cx="19" cy="55" r="15" fill="#949494" fill-opacity="0.5" />
+    <circle id="smoke2" cx="19" cy="35" r="15" fill="#949494" fill-opacity="0.5" />
+    <circle id="smoke1" cx="19" cy="15" r="15" fill="#949494" fill-opacity="0.5" />
+  </g>
+  <g>
+    <path d="M3.03333 60H35.5333L39 159H0L3.03333 60Z" fill="#D9D9D9" />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M1.62391 106L2.14478 89H36.5488L37.1441 106H1.62391Z"
+      fill="#EE1E1E"
+    />
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M36.0586 75H2.57374L3.03333 60H35.5333L36.0586 75Z"
+      fill="#EE1E1E"
+    />
+  </g>
+</svg>
+
+<style>
+  #smoke3 {
+    animation: fadeUpLower 1s linear infinite;
+    opacity: 0;
+  }
+  #smoke2 {
+    animation: fadeUpMiddle 1s linear infinite;
+    opacity: 1;
+  }
+  #smoke1 {
+    animation: fadeUpTop 1s linear infinite;
+    opacity: 1;
+  }
+  @keyframes fadeUpLower {
+    to {
+      opacity: 1;
+      transform: translateY(-20px);
+    }
+  }
+  @keyframes fadeUpMiddle {
+    to {
+      transform: translateY(-20px);
+    }
+  }
+  @keyframes fadeUpTop {
+    to {
+      opacity: 0;
+      transform: translateY(-20px);
+    }
+  }
+</style>
